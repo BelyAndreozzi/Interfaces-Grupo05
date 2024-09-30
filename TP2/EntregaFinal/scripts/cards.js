e435adfce0d7fd30fb09d1b78f67af058be88874
Belen Andreozzi <fbelen.andreozzi@gmail.com>
2024-09-30 17:59:32 -0300

    Íconos categoría. Sidebar y Cards WIP

--- TP2/EntregaFinal/scripts/cards.js
+++ TP2/EntregaFinal/scripts/cards.js
@@ -26,12 +26,42 @@ let games = [
         "precio": 22,
         "es_gratis": false,
         "categoria": "Acción"
+    },
+    {
+        "imagen": "src/cards/cat-life-simulator.avif",
+        "nombre_del_juego": "Cat Life Simulator",
+        "precio": 0.00,
+        "es_gratis": true,
+        "categoria": "Destacados"
+    },
+    {
+        "imagen": "src/cards/cat-life-simulator.avif",
+        "nombre_del_juego": "Cat Life Simulator",
+        "precio": 29.99,
+        "es_gratis": false,
+        "categoria": "Destacados"
+    },
+    {
+        "imagen": "src/cards/cat-life-simulator.avif",
+        "nombre_del_juego": "Cat Life Simulator",
+        "precio": 46,
+        "es_gratis": false,
+        "categoria": "Destacados"
+    },
+    {
+        "imagen": "src/cards/cat-life-simulator.avif",
+        "nombre_del_juego": "Cat Life Simulator",
+        "precio": 22,
+        "es_gratis": false,
+        "categoria": "Destacados"
     }
 ];
+
 
 let gameCategories = [
     "Acción",
     "Aventura",
+    "Destacados",
     "Estrategia",
     // "Deportes",
     // "Carreras",
@@ -43,29 +73,29 @@ let gameCategories = [
 ];
 
 createCategories(gameCategories);
+// createFeaturedCards(featuredGames);
 
-function createCategories(gameCategories){
+function createCategories(gameCategories) {
     let categoriesContainer = document.querySelector('.game-categories');
     gameCategories.forEach(category => {
-    categoriesContainer.innerHTML += 
-    `
+        categoriesContainer.innerHTML +=
+            `
             <section class="category" id="category-${category}">
                 <h2 class="category-name"> ${category} </h2>
                 <div  class="cards" id="${category}-cards">
                 </div>
             </section>
     `
-    createCards(games, category);
+        createCards(games, category);
     });
-
 }
 
 
 
-function createCards(games, category){
+function createCards(games, category) {
     let cardsContainer = document.getElementById(`${category}-cards`)
     games.forEach(game => {
-        if(category.toLowerCase() === game.categoria.toLowerCase()){
+        if (category.toLowerCase() === game.categoria.toLowerCase()) {
             let textPrice;
             let textBtn;
             if (game.es_gratis) {
@@ -76,8 +106,8 @@ function createCards(games, category){
                 textPrice = `$${game.precio}`;
                 textBtn = "Comprar";
             }
-            cardsContainer.innerHTML += 
-            `
+            cardsContainer.innerHTML +=
+                `
             <div class="card" id="card-${game.nombre_del_juego}">
                         <img class="card-image" src="${game.imagen}" alt="${game.nombre_del_juego}">
                         <div class="card-description">
@@ -89,6 +119,41 @@ function createCards(games, category){
                         </div>
                     </div>    
             `
+            if(game.categoria==Destacados){
+                document.querySelector(`#card-${game.nombre_del_juego}`).classList.add("featuredCard")
+                document.querySelector(`#card-${game.nombre_del_juego}`).classList.remove("Card")
+            }
         }
     });
 }
+
+function createFeaturedCards(featuredGames) {
+    let featuredCardsContainer = document.getElementById('featured-cards')
+    featuredGames.forEach(game => {
+        let textPrice;
+        let textBtn;
+        if (game.es_gratis) {
+            textPrice = "Gratis";
+            textBtn = "Jugar";
+
+        } else {
+            textPrice = `$${game.precio}`;
+            textBtn = "Comprar";
+        }
+        featuredCardsContainer.innerHTML +=
+            `
+                <div class="card" class="featured-card">
+                            <img class="card-image" src="${game.imagen}" alt="${game.nombre_del_juego}">
+                            <div class="card-description">
+                                <h3 class="card-name">${game.nombre_del_juego}</h3>
+                                <div class="card-bottom-container">
+                                    <p class="card-price">${textPrice}</p>
+                                    <button class="card-btn">${textBtn}</button> 
+                                </div>
+                            </div>
+                        </div>    
+                `
+        
+    });
+}
+
