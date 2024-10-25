6cba3ba4520a5c42953a0e388819646a150e7e5e
Carolina Vytas <00caro2002@gmail.com>
2024-10-25 14:18:47 -0300

    Se hizo el movimiento de las fichas.

--- TP3/EntregaFinal/scripts/game/Ficha.js
+++ TP3/EntregaFinal/scripts/game/Ficha.js
@@ -1,16 +1,101 @@
-class Ficha extends Figura {
-    constructor(x, y, color, ctx,) {
-        super(x, y, color, ctx);
+class Ficha {
+    constructor(posX, posY, ctx, radius, imgJugador) {
+        this.posX = posX;
+        this.posY = posY;
+        this.ctx = ctx;
+        this.radius = radius;
+        this.fill = imgJugador;
+        this.resaltado = false;
     }
 
-    draw() {
-        this.ctx.beginPath();
-        this.ctx.fillStyle = this.fill;
-        this.ctx.arc(this.posX, this.posY, 20, 0, 2 * Math.PI);
+    /* draw() {
+        console.log("draw cl " + this.fill);
+        this.ctx.beginPath();        
+        var img = ctx.createPattern(this.fill,"no-repeat");
+        this.ctx.fillStyle = img;
+        this.ctx.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
+        
         this.ctx.fill();
         this.ctx.closePath();
         this.ctx.lineWidth = 3;
         this.ctx.strokeStyle = "green";
         this.ctx.stroke();
+        // let imgg = new Image(20,20);
+        // imgg.src= `${this.img}`;
+        // this.ctx.fillImage(imgg);
+
+        if(this.resaltado===true){
+            this.ctx.strokeStyle = "red";
+            this.ctx.lineWidth = 5;
+            this.ctx.stroke();
+        }
+    } */
+
+    draw() {
+        console.log("draw cl " + this.fill);
+
+        // Dibuja el círculo como fondo
+        this.ctx.beginPath();
+        this.ctx.arc(this.posX, this.posY, this.radius, 0, 2 * Math.PI);
+        this.ctx.fillStyle = "white"; // Puedes cambiar el color de fondo si lo deseas
+        this.ctx.fill();
+        this.ctx.closePath();
+
+        // Dibuja la imagen dentro del círculo
+        this.ctx.drawImage(this.fill, this.posX - this.radius, this.posY - this.radius, this.radius * 2, this.radius * 2);
+
+        // Dibuja el borde del círculo
+        this.ctx.lineWidth = 3;
+        this.ctx.strokeStyle = "green";
+        this.ctx.stroke();
+
+        // Resaltado
+        if (this.resaltado === true) {
+            this.ctx.strokeStyle = "red";
+            this.ctx.lineWidth = 5;
+            this.ctx.stroke();
+        }
     }
+
+    isPointInside(x, y) {
+        let _x = this.posX - x;
+        let _y = this.posY - y;
+        let resultado = Math.sqrt(_x * _x + _y * _y) < this.radius;
+        return resultado;
+    }
+
+    getPosition() {
+        return { x: this.posX, y: this.posY };
+    }
+
+    getPosY() {
+        return this.posY;
+    }
+
+    getPosX() {
+        return this.posX;
+    }
+
+    getFill() {
+        return this.fill;
+    }
+
+    getFill() {
+        return this.resaltado;
+    }
+
+    setFill(fill) {
+        this.fill = fill;
+    }
+
+    setPosition(x, y) {
+        this.posX = x;
+        this.posY = y;
+    }
+
+    setResaltado(bool) {
+        this.resaltado = bool;
+    }
+
+
 }
--- TP3/EntregaFinal/scripts/game/Game.js
+++ TP3/EntregaFinal/scripts/game/Game.js
@@ -1,35 +1,98 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
 
+let fichas = [];
+let fichasPlayer1 = [];
+let fichasPlayer2 = [];
+let tablero;
+let modoDeJuego;
 
-let figuras = []
+let lastFigureClicked = null;
+let isMouseDown = false;
 
-/* function addFiguras(){
-    for(let i = 0; i < 10; i++){
-        let posX = Math.round(Math.random() * canvas.width);
-        let posY = Math.round(Math.random() * canvas.height);
-        let ficha = new Ficha(posX,posY,"red",ctx);
-        figuras.push(ficha);
-    }
-} */
-
-function addFiguras(wt, color) {
-    for (let i = 0; i < 10; i++) {
-        let posX = Math.round(wt);
-        let posY = Math.round((canvas.height - 50) - (20*i));
-        let ficha = new Ficha(posX, posY, color, ctx);
-        figuras.push(ficha);
-    }
+function addFiguras(wt, imgJugador) {
+    console.log("addFiguras", imgJugador);
+     for (let i = 0; i < 10; i++) {
+        let posX = wt;
+        let posY = 100 + (i * 30);
+        let ficha = new Ficha(posX, posY, ctx, 20, imgJugador);
+        fichas.push(ficha);
+     }
 }
 
 function drawFiguras() {
-    for (let i = 0; i < figuras.length; i++) {
-        figuras[i].draw();
+    clearCanvas();
+    for (let i = 0; i < fichas.length; i++) {
+        fichas[i].draw();
+    }
+} 
+
+function clearCanvas() {
+    ctx.fillStyle = "#1C1F3E";
+    ctx.fillRect (0,0,canvasWidth,canvasHeight);
+}
+
+canvas.addEventListener('mousedown', (e) => {
+    isMouseDown = true;
+
+    if (lastFigureClicked != null) {
+        lastFigureClicked.setResaltado(false);
+        lastFigureClicked = null;
+    }
+
+    let clickFig = findClickedFigure(e.offsetX, e.offsetY);
+    if (clickFig != null) {
+        clickFig.setResaltado(true);
+        lastFigureClicked = clickFig;
+    }
+    drawFiguras();
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if(isMouseDown && lastFigureClicked != null) {
+        lastFigureClicked.setPosition(e.offsetX, e.offsetY);
+        drawFiguras();
+    }
+})
+
+canvas.addEventListener('mouseup', (e) => {
+    isMouseDown = false;
+})
+
+function findClickedFigure(x, y){
+    for (let i = 0; i < fichas.length; i++) {
+        const element = fichas[i];
+        if (element.isPointInside(x, y)) {
+            return element;
+        }
     }
 }
 
-addFiguras(300 ,"red");
-addFiguras(600, "pink");
-drawFiguras();
+function deployGame(jugador1Img, jugador2Img) {
+    console.log(jugador1Img, jugador2Img);
+    addFiguras(100, jugador1Img);
+    addFiguras(500, jugador2Img);
+    drawFiguras();
+}
+
+//ACCIONES
+
+//deployMain();
+
+let imgJug1 = new Image(50,50);
+imgJug1.src = "src/games/4_en_linea.webp";
+let imgJug2 = new Image();
+imgJug2.src = "src/games/4_en_linea_2.webp";
+
+imgJug1.onload = () => {
+    imgJug2.onload = () => {
+        deployGame(imgJug1, imgJug2);
+    };
+};
 
 
+
+// canvas.addEventListener('mouseup', onMouseUp, false);
+// canvas.addEventListener('mousemove', onMouseMove, false);
